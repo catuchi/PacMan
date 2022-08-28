01bd76f70f6fdb94b4abb8f00172274166d1d0a6
Chibu <atuchibueze@gmail.com>
2022-08-27 21:36:27 -0400

    implement ghost move function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { LEVEL, OBJECT_TYPE } from "./setup";
+import { randomMovement } from "./ghostMoves";
 
 // Classes
 import GameBoard from "./GameBoard";
 import Pacman from "./Pacman";
+import Ghost from "./Ghost";
 
 // DOM Elements
 const gameGrid = document.querySelector("#game");
@@ -22,8 +24,15 @@ let powerPillActive = false;
 let powerPillTimer = null;
 
 function gameOver(pacman, grid) {}
+
 function checkCollision(pacman, ghosts) {}
-function gameLoop(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {
+  gameBoard.moveCharacter(pacman);
+
+  ghosts.forEach((ghost) => gameBoard.moveCharacter(ghost));
+}
+
 function startGame() {
   gameWin = false;
   powerPillActive = false;
@@ -38,6 +47,15 @@ function startGame() {
   document.addEventListener("keydown", (e) =>
     pacman.handleKeyInput(e, gameBoard.objectExist)
   );
+
+  const ghosts = [
+    new Ghost(5, 188, randomMovement, OBJECT_TYPE.BLINKY),
+    new Ghost(4, 209, randomMovement, OBJECT_TYPE.PINKY),
+    new Ghost(3, 230, randomMovement, OBJECT_TYPE.INKY),
+    new Ghost(2, 251, randomMovement, OBJECT_TYPE.CLYDE),
+  ];
+
+  timer = setInterval(() => gameLoop(pacman, ghosts), GLOBAL_SPEED);
 }
 
 // Initialize game
